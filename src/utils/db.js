b30a5d998e25210ad88b0b292b0e66eb94d68cfc
ajay potfode <ajaypotfode3277@gmail.com>
2024-11-05 15:01:35 +0530

    added .env file in project

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,4 +4,4 @@
 // const password="coder2002"
 
 // export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.l7rrp.mongodb.net/menudataDB?retryWrites=true&w=majority&appName=Cluster0"
-export const connectionStr=process.env.MONGO_URI
+export const connectionStr=process.env.CONNECTION_STR
